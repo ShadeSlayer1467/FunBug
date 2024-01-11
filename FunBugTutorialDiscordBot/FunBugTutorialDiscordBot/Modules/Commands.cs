10ea1dc393d353851833ee30694ba341cc02e6b8
Matthew Read <mattheweread@gmail.com>
2024-01-10 23:33:46 -0800

    Basic Working !ping command along with a working delete all messages command to wipe channel for testing purposes

---
+++ FunBugTutorialDiscordBot/FunBugTutorialDiscordBot/Modules/Commands.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+
+namespace FunBugTutorialDiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong!");
+        }
+        [Command("deltaalpha")]
+        public async Task DeleteAll()
+        {
+            var channel = Context.Channel;
+            var messages = await channel.GetMessagesAsync().FlattenAsync();
+            await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
+
+            //send delete count
+            var deleteCount = messages.Count();
+        }
+    }
+}

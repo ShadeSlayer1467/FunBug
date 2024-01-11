18c15af91171b878f9aa323bbdc14c5325769650
Matthew Read <mattheweread@gmail.com>
2024-01-10 23:33:46 -0800

    Basic App with !ping
    Also added a delete command to help clean up my testing discord channel.

---
+++ FunBugTutorialDiscordBot/FunBugTutorialDiscordBot/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FunBugTutorialDiscordBot
+{
+    internal class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private static DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            var JSONReader = new config.JSONReader();
+            await JSONReader.ReadJSON();
+
+            var config = new DiscordSocketConfig()
+            {
+                // Other config options can be presented here.
+                GatewayIntents = GatewayIntents.All
+            };
+            _client = new DiscordSocketClient(config);
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+            string DISCORD_FUNBUG_TOKEN = JSONReader.token;
+            _client.Log += _client_Log;
+            await RegisterCommandsAsync();
+            await _client.LoginAsync(TokenType.Bot, DISCORD_FUNBUG_TOKEN);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task _client_Log(LogMessage message)
+        {
+            Console.WriteLine(message);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private Task HandleCommandAsync(SocketMessage arg)
+        {
+            _ = Task.Run(async () =>
+            {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            const string PREFIX = "!";
+            if (message.HasStringPrefix(PREFIX, ref argPos))
+            {
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+            else
+            {
+                await context.Channel.SendMessageAsync("Unknown command. Type !help for a list of commands.");
+                // repeat message
+                await context.Channel.SendMessageAsync("\'" + message.Content + "\'");
+                }
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
